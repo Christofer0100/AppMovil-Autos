49c94403f21bf18a91bb904d8daa330badf97d84
Christofer0100 <ch.lagos@duocuc.cl>
2023-11-23 21:42:44 -0300

    import apk + correo conductor

--- README.txt.txt
+++ README.txt.txt
@@ -1,1 +1,10 @@
 npm install
+
+
+ionic build (instalar android studio en proyecto)
+
+ionic capacitor open android (Abrir proyecto en android studio)
+
+npx cap sync android (Crear carpetas en proyecto)
+
+
--- src/app/pasajero/pasajero.page.ts
+++ src/app/pasajero/pasajero.page.ts
@@ -10,6 +10,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 export class PasajeroPage implements OnInit {
 
   conductores: any[] = [];
+
   correoUsuario = localStorage.getItem('credentials') || '';
 
   latitud: number | undefined;
@@ -17,32 +18,35 @@ export class PasajeroPage implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  async obtenerCoordenadas() {
-
-    const obtenerCoordenadas = await Geolocation.getCurrentPosition()
-    this.latitud = obtenerCoordenadas.coords.latitude;
-    this.longitud = obtenerCoordenadas.coords.longitude;
-  }
-
-  enviarCorreo() {
-    const destinatario = this.correoUsuario;
-    console.log(destinatario)
-    const asunto = 'Asunto del correo';
-    const cuerpo = 'Cuerpo del correo...';
-
-    // Genera el enlace 'mailto'
-    const mailtoLink = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
-
+  async enviarCorreo(destinatarioConductor: string, destinatarioUsuario: string) {
+    console.log(destinatarioConductor);
+    console.log(destinatarioUsuario);
+    const asunto = 'Confirmar Reserva';
+    
+    const obtenerCoordenadas = await Geolocation.getCurrentPosition();
+    const latitud = obtenerCoordenadas.coords.latitude;
+    const longitud = obtenerCoordenadas.coords.longitude;
+  
+    // Construye el cuerpo del mensaje con saltos de línea
+    const cuerpo = `Correo de confirmación reserva viaje. Dirección Pasajero: ${latitud}, ${longitud}`;
+  
+    // Genera el enlace 'mailto' con el destinatario específico
+    const mailtoLink = `mailto:${destinatarioConductor},${destinatarioUsuario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
+  
     // Abre el cliente de correo predeterminado del usuario
     window.location.href = mailtoLink;
   }
 
 
-
+  async obtenerCoordenadas() {
+    const obtenerCoordenadas = await Geolocation.getCurrentPosition()
+    this.latitud = obtenerCoordenadas.coords.latitude;
+    this.longitud = obtenerCoordenadas.coords.longitude;
+  }
 
   ngOnInit() {
     // Realizar la solicitud HTTP para obtener la lista de conductores
-    this.http.get<any[]>('http://127.0.0.1:8000/api/conductores').subscribe(
+    this.http.get<any[]>('https://9wt8hjgn-8000.brs.devtunnels.ms/api/conductores').subscribe(
       conductores => {
         this.conductores = conductores;
       },
